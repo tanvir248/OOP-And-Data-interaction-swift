2fd72bf4eecda5a32f93cadd7b4830956283194b
Tanvir Rahman <60251246+tanvir248@users.noreply.github.com>
2024-02-29 14:18:58 +0600

    Demo Delegate system created for data passwing between two view

--- OOP & Data Transfer.playground/Pages/Delegate & Protocols.xcplaygroundpage/Contents.swift
+++ OOP & Data Transfer.playground/Pages/Delegate & Protocols.xcplaygroundpage/Contents.swift
@@ -1,5 +1,36 @@
-import UIKit
 
-var greeting = "Hello, playground"
-print(greeting)
+//Delegate & protocols data transfer
+import Foundation
 
+protocol CounterDelegate {
+    func increment() -> Void
+}
+
+class Counter: CounterDelegate {
+    private var value: Int = 0 {
+        didSet { print("Counter value: \(value)") }
+    }
+    
+    func increment() {
+        self.value += 1
+    }
+}
+
+class Control {
+    private var delegate: CounterDelegate
+    
+    init(delegate: CounterDelegate) {
+        self.delegate = delegate
+    }
+    
+    func buttonClicked() {
+        self.delegate.increment()
+    }
+}
+
+let counter = Counter()
+let control = Control(delegate: counter)
+
+control.buttonClicked()
+// Counter value: 1
+control.buttonClicked()
