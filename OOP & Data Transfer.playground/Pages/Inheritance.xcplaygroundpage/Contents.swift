878c465b0b6855706c7b1466f11c29337c35ab30
tanvir248 <tanvir27.58@gmail.com>
2024-03-09 23:04:14 +0600

    simple inheritance started

--- OOP & Data Transfer.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
+++ OOP & Data Transfer.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
@@ -2,7 +2,39 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+class person {
+    var name: String
+    var color: String
+    var height: Float
+    var maritalStatus: String
+    var age: String
+    var profession: String
+    
+    init(name: String, color: String, height: Float, maritalStatus: String, profession: String, age: String) {
+        self.name = name
+        self.color = color
+        self.height = height
+        self.maritalStatus = maritalStatus
+        self.profession = profession
+        self.age = age
+    }
+}
+class introvert: person {
+    let serious = "And then the introverts arrived"
+    init() {
+        super()
+    }
+    func introvertArrivedToProtest {
+        print("You know things are serious when the introverts arrive")
+    }
+}
 
-//: [Next](@next)
-print(greeting)
+class drugAddicted: person {
+    init() {
+        super()
+    }
+}
+
+var nexdecade: [introvert]
+var drugaddict: drugAddicted
+
