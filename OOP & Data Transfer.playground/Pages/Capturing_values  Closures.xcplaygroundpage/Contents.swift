15c890a54d552f2ec3f257f8a2d2edd78ea265ce
Tanvir Rahman <t.rahman@nexdecade.com>
2024-03-19 14:09:13 +0600

    Arrays minimum & maximum values

--- OOP & Data Transfer.playground/Pages/Capturing_values  Closures.xcplaygroundpage/Contents.swift
+++ OOP & Data Transfer.playground/Pages/Capturing_values  Closures.xcplaygroundpage/Contents.swift
@@ -4,4 +4,25 @@ import Foundation
 
 var greeting = "Hello, playground"
 
+print(greeting)
 //: [Next](@next)
+
+var arr: [Int] = [10, 8 ,-5, 7, 11, 14, 0, 10]
+
+var min = arr[0]
+
+for i in arr {
+    if i < min {
+        min = i
+    }
+}
+print("Minimum number: \( min)")
+
+
+
+for i in 0..<arr.count {
+    if arr[i] < 1 {
+       arr[i] = 1
+    }
+}
+print(arr)
