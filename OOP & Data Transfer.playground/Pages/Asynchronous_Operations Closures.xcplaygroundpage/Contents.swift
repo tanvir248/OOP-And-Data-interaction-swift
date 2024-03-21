63aadba1bb77d8fb091188d5ad6c07822ca671eb
Tanvir Rahman <t.rahman@nexdecade.com>
2024-03-21 10:46:54 +0600

    completion handler & escaping closures

--- OOP & Data Transfer.playground/Pages/Asynchronous_Operations Closures.xcplaygroundpage/Contents.swift
+++ OOP & Data Transfer.playground/Pages/Asynchronous_Operations Closures.xcplaygroundpage/Contents.swift
@@ -1,20 +1,33 @@
 //Async operation or completion handler or @escaping
 
-//func givenData(text: String, handler: @escaping(String) -> Void ) {
-//    handler("Hello \(text)")
-//}
-//
-//let data: () = givenData(text: "World") { str in
-//    print(str)
-//}
+func givenData(text: String, handler: @escaping(String) -> Void ) {
+    handler("Hello \(text)")
+}
 
-func greeting(completon: @escaping(String) -> Void) -> Int {
+let data: () = givenData(text: "World") { str in
+    print(str)
+}
+
+func greeting(completon: @escaping(String) -> Void) {
   
     completon("Hello")
 
-    return 0
 }
 
-let hello = greeting { str in
-    print(str)
+let hello: () = greeting { str in
+    print("greeting \(str)")
+}
+//Get data from closures @escaping without return anythings
+//Condtional completion
+
+func numberData(num: Int , completion: @escaping(String) -> Void) {
+    if num % 2 == 0 {
+        completion("Even Number")
+    }else {
+        completion("Odd Number")
+    }
+}
+
+numberData(num: 100) { numberStaus in
+    print(numberStaus)
 }
