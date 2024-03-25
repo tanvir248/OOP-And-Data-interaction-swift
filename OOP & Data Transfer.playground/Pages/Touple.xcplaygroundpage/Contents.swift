2e073730dc57f9264953ab0a54d1f01cd1bfd82c
Tanvir Rahman <t.rahman@nexdecade.com>
2024-03-25 10:15:02 +0600

    Tuple in swift

---
+++ OOP & Data Transfer.playground/Pages/Touple.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+// Example of touple
+
+var person = ("Tanvir", 24, 1.7, "Engineer")
+
+print("Name: \(person.0)")
+print("Age: \(person.1)")
+print("Height: \(person.2)m")
+print("Occupation: \(person.3)")
