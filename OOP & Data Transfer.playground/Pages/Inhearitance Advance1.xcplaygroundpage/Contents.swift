f4fc33503bcee3dedc35c72f112fd4a0156b9a67
Tanvir Rahman <t.rahman@nexdecade.com>
2024-06-02 18:09:56 +0600

    changes

--- OOP & Data Transfer.playground/Pages/Inhearitance Advance1.xcplaygroundpage/Contents.swift
+++ OOP & Data Transfer.playground/Pages/Inhearitance Advance1.xcplaygroundpage/Contents.swift
@@ -1,13 +1,18 @@
 // Advance inheritance RND
 
 class BTViewController {
+    var changeTracker = ""
     var searchPlaceholderText = "Search" {
         didSet {
             print("Changed text = \(searchPlaceholderText)")
+            changeTracker = searchPlaceholderText
         }
     }
     func changeSearchPlaceHolder(text: String) {
         searchPlaceholderText = text
+    }
+    func printChange() {
+        print(changeTracker)
     }
 }
 class AuthorDetailsAudioBook: BTViewController {
@@ -27,4 +32,5 @@ print(obj.searchPlaceholderText)
 var obj2 = BTViewController()
 obj.changeSearchPlaceHolder(text: "kabbik audioBook")
 obj2.changeSearchPlaceHolder(text: "kabbik audioBook2")
+
 print(obj2.searchPlaceholderText)
